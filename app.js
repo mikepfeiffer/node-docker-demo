08e21cdc14ea78f476fedb647dc3c832651a9b9d
mikepfeiffer <mike.pfeiffer@gmail.com>
2018-12-24 14:37:37 -0700

    updated app

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World v2!');
+  res.send('Hello World!');
 });
 
 app.listen(config.port, () => {
