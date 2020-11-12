9790bb8ab1625ccf22379dc0e6dec6f0d11f9ab2
Mike Pfeiffer <mike.pfeiffer@gmail.com>
2020-11-12 06:35:42 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!!!');
+  res.send('Hello Azure!!!');
 });
 
 app.listen(config.port, () => {
