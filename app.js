67b236f316e597116c4a62efa215294e9e73b430
Bhavyadua1994 <74084981+Bhavyadua1994@users.noreply.github.com>
2021-05-18 17:44:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Azure!!!');
+  res.send('Hello Azure version 2!!!');
 });
 
 app.listen(config.port, () => {
